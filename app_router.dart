aa7a95bd1008dc585ec45f58bdf5636f6c6727a9
MAXIPR344 <48794824@est.ort.edu.ar>
2026-06-04 15:25:45 -0300

    Create app_router.dart

---
+++ app_router.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_application_1/screens/product.dart';
+import 'package:flutter_application_1/screens/app.dart';
+import 'package:flutter_application_1/screens/login.dart';
+import 'package:flutter_application_1/screens/results.dart';
+import 'package:flutter_application_1/usuario.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter appRouter = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const LoginPage(),
+    ),
+    GoRoute(
+      path: '/app',
+      builder: (context, state) {
+        final usuario = state.extra as Usuario;
+        return AppScreen(usuario: usuario);
+      },
+    ),
+    GoRoute(
+      path: '/results',
+      builder: (context, state) {
+        final primerProducto = state.extra as Product;
+        return ResultsScreen(primerProducto: primerProducto);
+      },
+    ),
+  ],
+);
