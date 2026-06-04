8d69b5ac62b96295fd8f121510bb04b1c0054443
MAXIPR344 <48794824@est.ort.edu.ar>
2026-06-04 15:27:44 -0300

    Create usuario.dart

---
+++ usuario.dart
@@ -0,0 +1,13 @@
+class Usuario {
+  final String username;
+  final String password;
+  final String nombre;
+  final String apellido;
+
+  Usuario({
+    required this.username,
+    required this.password,
+    required this.nombre,
+    required this.apellido,
+  });
+}
